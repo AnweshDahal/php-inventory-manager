d75c7338a4ad1da8bf9957c842a628a11f387195
AnweshDahal <anweshd2000@gmail.com>
2021-07-04 21:40:28 +0545

    Adds SQL Dump and Updates Readme

--- Readme.md
+++ Readme.md
@@ -8,11 +8,34 @@ A simple Inventory Management webapplication built using PHP and MySQL.
 
 ## Installation
 
-Requires XAMPP or similar software
+Requires XAMPP or similar software, the instructions are set according to XAMPP' method
 
 1. Download the project's zip folder.
 2. Copy the contents into the htdocs folder of your XAMPP installaton
    `C:\XAMPP\htdocs\`
+
+### Installing the database
+
+1. Run the MySQL server in XAMPP.
+2. In your browse go to address `localhost/phpmyadmin`
+
+Default Method
+
+3. Create a database called `clausia_inventory`.
+4. Go to the import tab and import the **clausia_inventory.sql** file.
+5. Leave everything to default, scroll down to the bottom and click on the **Go** button.
+
+Custom Method
+
+3. Create a databse and give it your own name.
+4. Follow steps **4** and **5** from the Default Method.
+5. Go to the file `model/database.php` and replace the databse name in line 3 after the `dbname` parameter
+
+```php
+$dsn = "mysql:host=localhost;dbname=clausia_inventory";
+```
+
+6. If your MySQL server has a custom username and password, The defaults are `username = root` and no password, set the username in line 5 and the password in line 7.
 
 ## Features
 
---
+++ clausia_inventory.sql
@@ -0,0 +1,90 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 04, 2021 at 05:37 PM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `clausia_inventory`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `category_id` varchar(10) NOT NULL,
+  `category_name` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item`
+--
+
+CREATE TABLE `item` (
+  `item_id` int(11) NOT NULL,
+  `item_name` varchar(50) NOT NULL,
+  `manufacturer` varchar(50) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `category` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indexes for table `item`
+--
+ALTER TABLE `item`
+  ADD PRIMARY KEY (`item_id`),
+  ADD KEY `category` (`category`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `item`
+--
+ALTER TABLE `item`
+  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `item`
+--
+ALTER TABLE `item`
+  ADD CONSTRAINT `category` FOREIGN KEY (`category`) REFERENCES `category` (`category_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
